a1ce999e8317d5bfae92e451dfc46f0f6ce8c373
Austin Miller <austin.miller@colorado.edu>
2020-03-07 19:51:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,10 +18,11 @@ def get_japanese_emoticon(file_path, emoticon)
   lib["get_emoticon"].each do |eng_emo, jap_emo| 
   if emoticon == eng_emo
   response = jap_emo
-#else response = "Sorry, that emoticon was not found"
 end 
 end 
-  response
+if response == nil return "Sorry, that emoticon was not found"
+  else return response
+  end 
 end
 
 def get_english_meaning
